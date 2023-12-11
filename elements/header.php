ead4e8c201ea896d5b94d5ef3d8d3ba3fe1f7fad
Marcos Brito <marcos.tj.sp@hotmail.com>
2023-12-11 20:33:15 -0300

    versão 1.1 ainda iniciando

---
+++ elements/header.php
@@ -0,0 +1,7 @@
+<menu>
+    <ul>
+        <li><a href="index.php"><img src="galeria/icones/home.png"><div>Home</div></a></li>
+        <li><a href="index.php"><img src="galeria/icones/telefone.png"><div>Fale conosco</div></a></li>
+        <li><a href="index.php"><img src="galeria/icones/pasta.png"><div>Serviços</div></a></li>
+    </ul>
+</menu>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me Ajuda</title>
+    <link rel="stylesheet" href="css/style.scss">
+</head>
+
+<body id="body">
+    <header id="header">
+        <?php require "elements/header.php" ?>
+    </header>
+    <main id="corpo">
+        <h1>Transforme a velocidade do seu computador agora mesmo!</h1>
+        <p>
+            Potencialize a performance do seu computador com nosso serviço de upgrade para SSD. Desfrute de uma mudança extraordinária, ganhando velocidade e eficiência em cada tarefa. <a href="#">Clique e saiba mais!</a>
+        </p>
+        <p>"Estamos dedicados a auxiliá-lo em superar quaisquer desafios relacionados ao seu computador. Seja lidando com questões técnicas, otimizando o desempenho ou solucionando problemas específicos, nossa equipe está pronta para oferecer suporte especializado. Conte conosco para proporcionar uma experiência tranquila e eficaz, garantindo que seu computador funcione da melhor forma possível."</p>
+        <p>"Sua empresa demanda serviços de informática, e nós compreendemos a importância de equilibrar qualidade e custo. Entendemos que os preços de serviços de informática podem inicialmente parecer intimidantes. No entanto, nossa proposta é oferecer a você serviços de excelência a um preço justo, superando qualquer concorrência no mercado. Estamos comprometidos em fornecer soluções que atendam às suas necessidades específicas. Ao entrar em contato conosco, teremos o prazer de discutir suas necessidades e personalizar o melhor contrato possível, assegurando um investimento que valorize cada centavo do seu orçamento."
+        </p>
+        <p>
+            "Apesar de parecer contra intuitivo, é totalmente viável economizar sem comprometer a qualidade do serviço. Compreendemos que muitas empresas buscam maximizar seus lucros, por vezes negligenciando a satisfação do cliente. No entanto, diferenciamo-nos ao colocar o sincero interesse de nossos clientes em primeiro lugar. Nosso compromisso é proporcionar não apenas serviços de alta qualidade, mas também uma experiência que vá além das expectativas. Acreditamos que a satisfação do cliente é o verdadeiro indicador de sucesso, e estamos dedicados a garantir que nossos clientes estejam não apenas satisfeitos, mas genuinamente contentes com os serviços que oferecemos."
+        </p>
+        <article>
+            <p>Os SSDs e HDs são duas tecnologias de armazenamento com características distintas. Os HDs utilizam discos magnéticos giratórios para armazenar dados, enquanto os SSDs empregam memória flash sem partes móveis. Essa diferença fundamental tem um impacto significativo no desempenho.</p>
+
+            <p>Os SSDs oferecem velocidades de leitura e gravação muito superiores em comparação com os HDs. Enquanto os HDs dependem do movimento físico de uma cabeça de leitura para acessar dados, os SSDs não têm partes móveis, permitindo acesso quase instantâneo às informações. Isso se traduz em tempos de inicialização mais rápidos, carregamento rápido de aplicativos e uma resposta mais ágil do sistema operacional.</p>
+
+            <p>Além disso, os SSDs são notavelmente eficientes na transferência de dados, resultando em tempos de carregamento mais curtos para jogos e aplicativos pesados. A confiabilidade também é aprimorada, pois os SSDs são menos suscetíveis a danos físicos que podem afetar os HDs.</p>
+
+            <p> resumo, ao optar por um SSD, você experimentará uma significativa melhoria no desempenho do seu sistema, desfrutando de operações mais rápidas, maior eficiência e uma experiência geral mais fluida.</p>
+        </article>
+    </main>
+    <footer id="footer">
+        <?php require "elements/footer.php" ?>
+    </footer>
+</body>
+
+</html>
